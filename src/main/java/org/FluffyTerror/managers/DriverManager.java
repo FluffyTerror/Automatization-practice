1ac288960cf0914036d88e503bbd80708e8c9328
FluffyTerror <8353207@gmail.com>
2024-12-15 03:12:22 +0300

    ?

--- src/main/java/org/FluffyTerror/managers/DriverManager.java
+++ src/main/java/org/FluffyTerror/managers/DriverManager.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.FluffyTerror.utils.Const.*;
+import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class DriverManager {
 
@@ -126,7 +127,5 @@ public class DriverManager {
         selenoidOptions.put("enableVNC", true);
         selenoidOptions.put("enableVideo", false);
         capabilities.setCapability("selenoid:options", selenoidOptions);
-
-
     }
 }
